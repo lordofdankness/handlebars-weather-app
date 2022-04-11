b2b86b8092a94051e64faae01df759fc648d1349
lordofdankness <shehaj.karlo@gmail.com>
2022-04-11 22:35:05 +0300

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -10,8 +10,10 @@ const forrecast = require('./utils/forrecast');
 
 const app = express();
 
-app.listen(3000, () => {
-	console.log('Server is up on port 3000.');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}.`);
 });
 
 const puplicPath = path.join(__dirname, '../public');
