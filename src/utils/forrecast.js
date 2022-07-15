43011c6d8c0aa245a155a775eeaf582b4df8290c
lordofdankness <shehaj.karlo@gmail.com>
2022-07-15 21:43:47 +0300

    added env variable handling

--- src/utils/forrecast.js
+++ src/utils/forrecast.js
@@ -1,8 +1,10 @@
 const request = require('postman-request');
-const apiKeys = require('./keys');
+require('dotenv').config();
+
+const weatherStackApiKey = process.env[ 'weatherStackApiKey' ];
 
 const forrecast = async (latitude, longitude, callback) => {
-	const weatherStackURL = `http://api.weatherstack.com/current?access_key=${apiKeys.weatherStackApiKey}&query=${latitude},${longitude}&units=m`;
+	const weatherStackURL = `http://api.weatherstack.com/current?access_key=${weatherStackApiKey}&query=${latitude},${longitude}&units=m`;
 	request({ url: weatherStackURL, json: true }, (error, { body } = {}) => {
 		if (error) {
 			callback('Unable to connect to weather services.', undefined);
@@ -10,7 +12,7 @@ const forrecast = async (latitude, longitude, callback) => {
 			callback('Unable to find location.', undefined);
 		} else {
 			callback(undefined, {
-				weatherDescription: body.current.weather_descriptions[0],
+				weatherDescription: body.current.weather_descriptions[ 0 ],
 				temperature: body.current.temperature,
 				feelsLike: body.current.feelslike,
 			});
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,18 +1,20 @@
 const request = require('postman-request');
-const apiKeys = require('./keys');
+require('dotenv').config();
+
+const mapboxApiKey = process.env[ 'mapboxApiKey' ];
 
 const geocode = async (address, callback) => {
-	const mapboxURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${apiKeys.mapboxApiKey}&limit=1`;
+	const mapboxURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${mapboxApiKey}&limit=1`;
 	request({ url: mapboxURL, json: true }, (error, { body } = {}) => {
 		if (error) {
 			callback('Unable to connect to location services.', undefined);
-		} else if (body.features.length === 0) {
+		} else if (body.features?.length === 0) {
 			callback('Unable to find location. Try another search.', undefined);
 		} else {
 			callback(undefined, {
-				latitude: body.features[0].center[1],
-				longtitude: body.features[0].center[0],
-				location: body.features[0].place_name,
+				latitude: body.features[ 0 ].center[ 1 ],
+				longtitude: body.features[ 0 ].center[ 0 ],
+				location: body.features[ 0 ].place_name,
 			});
 		}
 	});
